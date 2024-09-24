216c905bccdc6aa5c014b75c8bc56446bde0eae2
Avinash Kadaji <kadaji@gmail.com>
2024-09-24 16:48:23 -0400

    report mod update

--- report/urls/highschool_admin.py
+++ report/urls/highschool_admin.py
@@ -12,8 +12,7 @@ app_name = 'highschool_admin_report'
 urlpatterns = [
     path('', reports, name='reports'),
     path('reports_in_category/', reports_in_category, name='reports_in_category'),
-    path('report_details/', report_details, name='report_details'),
-    path('run_report/<uuid:report_id>', run_report, name='run_report'),
+    path('report_details/', report_details, name='report_details'),    path('run_report/<uuid:report_scheduler_id>', run_report, name='run_report'),
 
     path('add_new', add_new_report, name='add_new'),
 ]
