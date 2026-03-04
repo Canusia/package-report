e78648abbe0e3ce84b8bb8a31e5900764546b691
Avinash Kadaji <kadaji@gmail.com>
2026-03-04 06:04:12 -0500

    add report scheduler for school admin

--- report/urls/faculty.py
+++ report/urls/faculty.py
@@ -1,22 +1,49 @@
 """
     Support Ticket CE URL Configuration
 """
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
 
 from ..views.report import (
     reports, reports_in_category,
     report_details, run_report,
     add_new as add_new_report,
+    schedule_report,
+    update_report,
     report_status_check,
+    ReportSchedulerViewSet,
+    run_command,
     download
 )
+
 app_name = 'faculty_report'
+
+router = routers.DefaultRouter()
+router_viewsets = {
+    'report_scheduler': ReportSchedulerViewSet
+}
+
+for router_key in router_viewsets.keys():
+    router.register(
+        router_key,
+        router_viewsets[router_key],
+        basename=router_key
+    )
+
 urlpatterns = [
+
+    path('api/', include(router.urls)),
+
     path('', reports, name='reports'),
     path('reports_in_category/', reports_in_category, name='reports_in_category'),
-    path('report_details/', report_details, name='report_details'),path('run_report/<uuid:report_scheduler_id>', run_report, name='run_report'),
-
+    path('report_details/', report_details, name='report_details'),
+    path('run_report/<uuid:report_scheduler_id>', run_report, name='run_report'),
     path('download/<uuid:report_scheduler_id>', download, name='download'),
+    path('run_command/<slug:command>', run_command, name='run_command'),
+
+    path('schedule_report/', schedule_report, name='schedule_report'),
+    path('update_report/', update_report, name='update_report'),
     path('status_check/', report_status_check, name='report_status_check'),
     path('add_new', add_new_report, name='add_new'),
 ]
