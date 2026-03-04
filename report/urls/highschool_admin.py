b97df5f4d17e59b83da58739ae81cae1e4abb26d
Avinash Kadaji <kadaji@gmail.com>
2026-03-04 05:56:48 -0500

    add report scheduler for school admin

--- report/urls/highschool_admin.py
+++ report/urls/highschool_admin.py
@@ -8,6 +8,7 @@ from ..views.report import (
     report_details, run_report,
     add_new as add_new_report,
     report_status_check,
+    schedule_report,
     download
 )
 app_name = 'highschool_admin_report'
@@ -16,6 +17,7 @@ urlpatterns = [
     path('reports_in_category/', reports_in_category, name='reports_in_category'),
     path('report_details/', report_details, name='report_details'),path('run_report/<uuid:report_scheduler_id>', run_report, name='run_report'),
 
+    path('schedule_report/', schedule_report, name='schedule_report'),
     path('download/<uuid:report_scheduler_id>', download, name='download'),
     path('status_check/', report_status_check, name='report_status_check'),
     path('add_new', add_new_report, name='add_new'),
